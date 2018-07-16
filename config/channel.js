5ec5ad070eabff0ef8ddbbb7c7e994c4183c248d
Taufan Mahaputra <hacker.indo62@gmail.com>
2018-07-16 20:43:06 +0700

    fix syntax

--- config/channel.js
+++ config/channel.js
@@ -1,2 +1,2 @@
-export const CHANNEL_ACCESS_TOKEN = process.env.CHANNEL_ACCESS_TOKEN
-export const CHANNEL_SECRET = process.env.CHANNEL_SECRET
+exports.CHANNEL_ACCESS_TOKEN = process.env.CHANNEL_ACCESS_TOKEN
+exports.CHANNEL_SECRET = process.env.CHANNEL_SECRET
