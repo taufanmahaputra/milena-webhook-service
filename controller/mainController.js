9f106f5e89b42979d8ffb9a9a92bff9d157a89df
Taufan Mahaputra <hacker.indo62@gmail.com>
2018-07-18 22:40:41 +0700

    add setup google calendar

--- controller/mainController.js
+++ controller/mainController.js
@@ -1,11 +1,15 @@
 const echoController = require('./echoController')
+const calendarController = require('./calendarController')
 
 exports.handleEvent = (client, event) => {
   if (event.type !== 'message' || event.message.type !== 'text') {
     // ignore non-text-message event
     return Promise.resolve(null)
   }
-
+  
+  if (event.message.text.toLowerCase() === '/init_calendar') {
+    return client.replyMessage(event.replyToken, calendarController.setupCalendar(event))
+  }
   // TODO: if your confidition, do return your result from your controller
 
   return client.replyMessage(event.replyToken, echoController.echoMessage(event))
