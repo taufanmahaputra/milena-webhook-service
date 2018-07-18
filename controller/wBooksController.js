7fe9876ec2b6a4e32f7f25de700e12d911bc460f
Taufan Mahaputra <hacker.indo62@gmail.com>
2018-07-18 19:47:50 +0700

    add logger

--- controller/wBooksController.js
+++ controller/wBooksController.js
@@ -20,6 +20,9 @@ exports.sendBookRecommendation = (client, event, bookData) => {
     var columns = carouselController.newColumn(book.imageLinks.thumbnail, '#FFFFFF', book.title, book.description, book.infoLink, actions)
     bookCarousel.template.columns.push(columns)
   })
+  console.log("WKWKKWKWKWKWKW");
+  console.log(bookCarousel);
+  console.log("WKWKKWKWKWKWKW");
   if (bookCarousel.template.columns.length <= 0) {
     return client.replyMessage(event.replyToken, defaultAnswer)
   }
