fa57543906034571a4b964b6f805352bd4e74255
harumlokawati <harumlokawati@yahoo.com>
2018-07-22 14:16:55 +0700

    merge

--- controller/wBooksController.js
+++ controller/wBooksController.js
@@ -4,6 +4,33 @@ const _ = require('lodash');
 const querystring = require('querystring');
 
 var BOOKS_API_BASE_URL = 'https://www.googleapis.com/books/v1'
+// https://developers.google.com/books/docs/v1/using#st_params
+var defaultOptions = {
+  // Google API key
+  key: null,
+  // Search in a specified field
+  field: null,
+  // The position in the collection at which to start the list of results (startIndex)
+  offset: 0,
+  // The maximum number of elements to return with this request (Max 40) (maxResults)
+  limit: 10,
+  // Restrict results to books or magazines (or both) (printType)
+  type: 'all',
+  // Order results by relevance or newest (orderBy)
+  order: 'relevance',
+  // Restrict results to a specified language (two-letter ISO-639-1 code) (langRestrict)
+  lang: 'en'
+};
+
+
+// Special Keywords
+var fields = {
+  title: 'intitle:',
+  author: 'inauthor:',
+  publisher: 'inpublisher:',
+  subject: 'subject:',
+  isbn: 'isbn:'
+};
 
 exports.search = (query, options, callback) => {
   // Make the options object optional
